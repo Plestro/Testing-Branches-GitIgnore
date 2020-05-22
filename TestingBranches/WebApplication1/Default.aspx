81a04842ba2b2c2960f4faa6f8505f90c0788caf
Keegan Launspach <keegan.launspach1@hotmail.com>
2020-05-22 15:19:23 +0200

    Commit NEW KEEGANE

--- TestingBranches/WebApplication1/Default.aspx
+++ TestingBranches/WebApplication1/Default.aspx
@@ -52,19 +52,7 @@
             </asp:GridView>
         </div>
 
-        <div class="col-md-4">
-            <h2>Customer</h2>
-            <asp:GridView ID="gvKeegan2" runat="server">
-
-            </asp:GridView>
-        </div>
-
-        <div class="col-md-4">
-            <h2>Customer</h2>
-            <asp:GridView ID="gvKeegan3" runat="server">
-
-            </asp:GridView>
-        </div>
+        
     </div>
 
 </asp:Content>
--- TestingBranches/WebApplication1/Default.aspx.cs
+++ TestingBranches/WebApplication1/Default.aspx.cs
@@ -25,7 +25,6 @@ namespace WebApplication1
                 LoadData();
             }
         }
-
         protected void LoadData()
         {
 
@@ -33,21 +32,15 @@ namespace WebApplication1
 
             DataTable dt = DAL.GetCustomerInfo();
             DataTable dg = DAL.GetPlestroInfo();
-            DataTable dg2 = DAL.GetPlestroInfo();
-            DataTable dg3 = DAL.GetPlestroInfo();
 
             gvCustomers.DataSource = dt.DefaultView;
             gvCustomers.DataBind();
 
+
             gvKeegan.DataSource = dg.DefaultView;
             gvKeegan.DataBind();
 
-            gvKeegan2.DataSource = dg2.DefaultView;
-            gvKeegan2.DataBind();
-
-            gvKeegan3.DataSource = dg2.DefaultView;
-            gvKeegan3.DataBind();
-
+          
 
             DAL.CloseConnection();
 
